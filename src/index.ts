ff6365d50290b3ed5591ce68af88a7417a6ff4de
Elex03 <eacuna042@gmail.com>
2024-08-22 00:20:29 -0600

    api: create controller auth

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, {Express, Request, Response} from 'express'
 import { PORT } from './config'
+import rootRouter from './routes'
+import { PrismaClient } from '@prisma/client'
+
 const app:Express = express()
 
+app.use(express.json())
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Listen on port 3000')
+app.use('/api', rootRouter)
 
+export const prismaClient = new PrismaClient({
+    log:['query']
 })
-
-
 app.listen(PORT, () => {console.log('App workinh on port 3000')})
