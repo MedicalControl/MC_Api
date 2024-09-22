95bce43970c6a2276e45a380fb99ade6ab5930a3
Elex03 <eacuna042@gmail.com>
2024-09-21 20:59:41 -0600

    documentation: Register and login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import {prismaClient} from '..';
-import {hashSync,   compareSync} from 'bcrypt'
-import * as jwt from 'jsonwebtoken'
+import { prismaClient } from "..";
+import { hashSync, compareSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
 import { JWT_ROUND, JWT_SECRET } from "../config";
 import { BadRequestException } from "../exceptions/bad-request";
 import { ErrorCode } from "../exceptions/root";
@@ -9,66 +9,103 @@ import { signUpSchema } from "../schemas";
 import { NotFoundException } from "../exceptions/not-found";
 import { user } from "@prisma/client";
 
-declare module 'express' {
-    interface Request {
-        user?: user;
-    }
+declare module "express" {
+  interface Request {
+    user?: user;
+  }
 }
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
-    signUpSchema.parse(req.body)
-    const {email, password , name, address, ocupation, bloodType, lastname, idCard, number, birthDate, inssnumber, sex, districtid, municipalityid} = req.body;
-    const birthDateC = new Date(birthDate)
-    let user = await prismaClient.user.findFirst({where: {email}})
-    let patient;
-    if (user) 
-        next (new BadRequestException('User alreay exists!', ErrorCode.USER_ALREADY_EXIST))
+const medicalRecord_Create = (municipalityid: any, districtid: any) =>
+  String(municipalityid + districtid);
 
-    patient = await prismaClient.patient.create({
-        data: {
-            name, 
-            lastname, 
-            address, 
-            birthDate: birthDateC,
-            bloodType, 
-            ocupation,
-            inssnumber, 
-            idCard,
-            sex, 
-            number, 
-            user: {
-                create: {
-                    password: hashSync(password, JWT_ROUND),
-                    email
-                }
-            }, 
-            municipality: {
-                connect: {id: municipalityid}
-            }, 
-            district: {
-                connect: {id: districtid}
-            }
-        }
-    })
-    res.json(patient)
-}
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  signUpSchema.parse(req.body);
+  const {
+    email,
+    password,
+    name,
+    address,
+    ocupation,
+    bloodType,
+    lastname,
+    idCard,
+    number,
+    birthDate,
+    inssnumber,
+    sex,
+    districtid,
+    municipalityid,
+  } = req.body;
+  const birthDateC = new Date(birthDate);
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  let patient;
+  if (user)
+    next(
+      new BadRequestException(
+        "User alreay exists!",
+        ErrorCode.USER_ALREADY_EXIST
+      )
+    );
+  const { healthunitid = 1 } = req.body;
+  patient = await prismaClient.patient.create({
+    data: {
+      name,
+      lastname,
+      address,
+      birthDate: birthDateC,
+      bloodType,
+      ocupation,
+      inssnumber,
+      idCard,
+      sex,
+      number,
+      user: {
+        create: {
+          password: hashSync(password, JWT_ROUND),
+          email,
+        },
+      },
+      municipality: {
+        connect: { id: municipalityid },
+      },
+      district: {
+        connect: { id: districtid },
+      },
+    },
+  });
+  res.json(patient);
+};
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-    const {email, password} = req.body;
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
 
-    let user = await prismaClient.user.findFirst({where: {email}})
-    console.log(user);
-    if (!user) 
-        throw  new NotFoundException('User not found', ErrorCode.USER_NOT_FOUND)
-    if (!compareSync(password, user.password))
-        throw new BadRequestException('Incorrect password!', ErrorCode.INCORRECT_PASSWORD)
-    const token = jwt.sign({
-        userId: user.id,
-    }, JWT_SECRET)
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  console.log(user);
+  if (!user)
+    throw new NotFoundException("User not found", ErrorCode.USER_NOT_FOUND);
+  if (!compareSync(password, user.password))
+    throw new BadRequestException(
+      "Incorrect password!",
+      ErrorCode.INCORRECT_PASSWORD
+    );
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  );
 
-    res.json({token})
-}
+  res.json({ token });
+};
 
 export const me = async (req: Request, res: Response, next: NextFunction) => {
-    res.json(req.user)
-}
+  res.json(req.user);
+};
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,37 +6,199 @@ import authMiddleware from '../middlewares/auth'
 
 const authRoutes: Router = Router()
 
-authRoutes.post('/signup', errorHandler(signup))
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Inicio de sesión de un usuario
+ *     description: Autentica a un usuario existente y genera un token JWT.
+ *     tags:
+ *       - Autenticación
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Correo electrónico del usuario.
+ *                 example: usuario@ejemplo.com
+ *               password:
+ *                 type: string
+ *                 description: Contraseña del usuario.
+ *                 example: contraseña123
+ *     responses:
+ *       200:
+ *         description: Inicio de sesión exitoso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: Token JWT generado para el usuario autenticado.
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+ *       400:
+ *         description: Error en la solicitud. Contraseña incorrecta.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error.
+ *                   example: "Incorrect password!"
+ *       404:
+ *         description: Usuario no encontrado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error.
+ *                   example: "User not found"
+ *       500:
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error del servidor.
+ *                   example: "Error al iniciar sesión."
+ */
 authRoutes.post('/login', errorHandler(login))
+
 authRoutes.get('/me', [authMiddleware], errorHandler(me))
 
 export default authRoutes
 
 /**
  * @swagger
- * /api/district/Municipality/{district}:
- *   get:
- *     summary: List of medicine
- *     security:
- *       - apiAuth: []
+ * /api/auth/signup:
+ *   post:
+ *     summary: Registro de un nuevo paciente
+ *     description: Registra un nuevo paciente en el sistema con los datos proporcionados en el cuerpo de la solicitud.
  *     tags:
- *       - Register
- *     parameters: 
- *      - in: path
- *        name: district
- *        required: true
- *        description: Id of district from department
- *        schema: 
- *          type: number
+ *       - Autenticación
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Correo electrónico del usuario.
+ *                 example: usuario@ejemplo.com
+ *               password:
+ *                 type: string
+ *                 description: Contraseña del usuario.
+ *                 example: contraseña123
+ *               name:
+ *                 type: string
+ *                 description: Nombre del usuario.
+ *                 example: Juan
+ *               lastname:
+ *                 type: string
+ *                 description: Apellido del usuario.
+ *                 example: Pérez
+ *               address:
+ *                 type: string
+ *                 description: Dirección del usuario.
+ *                 example: Calle Falsa 123
+ *               ocupation:
+ *                 type: string
+ *                 description: Ocupación del usuario.
+ *                 example: Ingeniero
+ *               bloodType:
+ *                 type: string
+ *                 description: Tipo de sangre del usuario.
+ *                 example: O+
+ *               idCard:
+ *                 type: string
+ *                 description: Número de identificación del usuario.
+ *                 example: 123456789
+ *               number:
+ *                 type: string
+ *                 description: Número de teléfono del usuario.
+ *                 example: +50512345678
+ *               birthDate:
+ *                 type: string
+ *                 format: date
+ *                 description: Fecha de nacimiento del usuario.
+ *                 example: 1990-01-01
+ *               inssnumber:
+ *                 type: string
+ *                 description: Número INSS del usuario.
+ *                 example: 987654321
+ *               sex:
+ *                 type: string
+ *                 description: Sexo del usuario.
+ *                 example: M
+ *               districtid:
+ *                 type: number
+ *                 description: ID del distrito al que pertenece el usuario.
+ *                 example: 1
+ *               municipalityid:
+ *                 type: number
+ *                 description: ID del municipio al que pertenece el usuario.
+ *                 example: 1
+ *               healthunitid:
+ *                 type: number
+ *                 description: ID de la unidad de salud a la que pertenece el usuario.
+ *                 example: 1
  *     responses:
  *       200:
- *         description: Get district OK
- *       401:
- *         description: No auth
- *       400: 
- *         description: Error in validation
- *       404: 
- *         description: No such district 
+ *         description: Registro exitoso del usuario.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: number
+ *                   description: ID del paciente registrado.
+ *                   example: 1
+ *                 email:
+ *                   type: string
+ *                   description: Correo electrónico del paciente registrado.
+ *                   example: usuario@ejemplo.com
+ *                 name:
+ *                   type: string
+ *                   description: Nombre del paciente registrado.
+ *                   example: Juan
+ *       400:
+ *         description: Error de validación.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error.
+ *                   example: "User already exists!"
  *       500:
- *         description: Error
+ *         description: Error en el servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error del servidor.
+ *                   example: "Error al registrar el usuario."
  */
+authRoutes.post('/signup', errorHandler(signup))
